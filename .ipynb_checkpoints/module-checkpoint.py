dfc81726d2347bc34669cfec22682f807271b416
jeiavonni <jeiavonni765@gmail.com>
2024-01-21 15:04:45 -0600

    Week 2 Day 4

---
+++ .ipynb_checkpoints/module-checkpoint.py
@@ -0,0 +1,11 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+x = 1
+def area(width,length):
+    answer = (width * length)
+    return answer
+
+def circ(r):
+    answer = (2*3.14*r)
+    return answer
